7d1640e96e4856d464ad9d221bccf20c34a22ee7
panyu <3082167807@qq.com>
2019-12-14 08:30:30 +0800

    “第一次提交”

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <router-view></router-view>
+    <FooterGuide v-show="$route.meta.isShowFooter"></FooterGuide>
+  </div>
+</template>
+
+<script>
+import FooterGuide from '@/components/FooterGuide/FooterGuide.vue'
+  export default {
+    components:{
+      FooterGuide
+    }
+  }
+</script>
+
+<style scoped>
+  
+</style>
---
+++ src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import 'lib-flexible'
+
+import App from './App.vue'
+import router from './router'
+import Header from './components/Header/Header.vue'
+
+Vue.component('Header',Header)
+
+new Vue({
+  render:h=>h(App),
+  router,
+}).$mount('#app')
